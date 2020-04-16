b9e1200bc5dfefc06df2612a35291e1120e6578b
Santiago Cadena <sa.cadena721@gmail.com>
2020-04-16 11:18:31 +0200

    add setup

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+setup(
+    name='ptrnets',
+    version='0.0.0',
+    description='Easy access to pretrained models for system identification',
+    author='Santiago Cadena',
+    author_email='santiago.cadena@uni-tuebingen.de',
+    packages=find_packages(exclude=[]),
+    install_requires=[],
+)
