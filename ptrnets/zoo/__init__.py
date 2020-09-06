549e3eb40733e718351f622658ef73587964490b
Santiago Cadena <sa.cadena721@gmail.com>
2020-09-06 17:22:38 -0500

    add untrained renset50

--- ptrnets/zoo/__init__.py
+++ ptrnets/zoo/__init__.py
@@ -3,3 +3,11 @@ from .shape_biased import *
 from .simclr import *
 #from .imnet_rnet import *
 from .robust import *
+
+import torchvision
+import torch 
+
+
+def resnet50_untrained(pretrained=False, seed=42, **kwargs):
+    torch.manual_seed(seed)
+    return torchvision.models.resnet50(pretrained=False, **kwargs)
