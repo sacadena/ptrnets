f9a8642de09ef665d7eef148733b425075c741a8
Santiago Cadena <sa.cadena721@gmail.com>
2022-07-04 00:45:03 -0500

    fixed bug

--- ptrnets/zoo/cornet.py
+++ ptrnets/zoo/cornet.py
@@ -4,7 +4,7 @@ except ModuleNotFoundError:
     import sys
     import subprocess
     python = sys.executable
-    missing ["git+https://github.com/dicarlolab/CORnet"]
+    missing = ["git+https://github.com/dicarlolab/CORnet"]
     subprocess.check_call([python, '-m', 'pip', 'install', *missing], stdout=subprocess.DEVNULL)
 
 from cornet import cornet_z, cornet_r, cornet_rt, cornet_s
